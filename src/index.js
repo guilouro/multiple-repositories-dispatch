ac1fb0d1d12f6efcd944d55920155f11354c08bf
Guilherme Louro <guipclouro@gmail.com>
2020-04-07 11:59:27 -0300

    [feature/actions] - Create multiple repositories dispatch action (#1)
    * Init project config
    
    * Add action config
    
    * Create dispatch action
    
    * update config
    
    * update repo
    
    * log post
    
    * add trim in item
    
    * log vars
    
    * fix action config
    
    * Add readme
    
    * update readme with receive event example
    
    * fix action
    
    * remove log

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const { request } = require('@octokit/request')
+const core = require('@actions/core')
+
+;(async function () {
+  try {
+    const token = core.getInput('repo_token')
+    const repositories = core.getInput('repositories').split('\n')
+    const eventType = core.getInput('event_type')
+    const payload = core.getInput('client_payload') || '{}'
+
+    repositories.forEach(async (repo) => {
+      try {
+        const post = `POST /repos/${repo.trim()}/dispatches`
+        await request(post, {
+          headers: { authorization: `token ${token}` },
+          mediaType: { previews: ['everest'] },
+          event_type: `${eventType}`,
+          client_payload: JSON.parse(payload)
+        })
+        console.log(`${post} was done!`)
+      } catch (error) {
+        core.setFailed(`${repo}: ${error.message}`)
+      }
+    })
+  } catch (error) {
+    core.setFailed(error.message)
+  }
+})()
---
+++ src/test.js
@@ -0,0 +1,52 @@
+const { request } = require('@octokit/request')
+const core = require('@actions/core')
+
+const mockParams = {
+  repo_token: '123',
+  repositories: 'guilouro/repo1 \nguilouro/repo2 \nguilouro/repo3',
+  event_type: 'my_event',
+  client_payload: '{ "test": "123"}'
+}
+
+jest.mock('@actions/core')
+jest.mock('@octokit/request')
+
+request.mockImplementation(() => jest.fn())
+core.getInput.mockImplementation((key) => mockParams[key])
+
+describe('Action', () => {
+  it('Should send post for repositories', () => {
+    require('./index')
+    expect(request).toBeCalledTimes(3)
+    expect(request.mock.calls[0][0]).toBe(
+      'POST /repos/guilouro/repo1/dispatches'
+    )
+    expect(request.mock.calls[1][0]).toBe(
+      'POST /repos/guilouro/repo2/dispatches'
+    )
+    expect(request.mock.calls[2][0]).toBe(
+      'POST /repos/guilouro/repo3/dispatches'
+    )
+  })
+
+  it('Should use repo_token in authorization header', () => {
+    require('./index')
+    expect(request.mock.calls[0][1]).toMatchObject({
+      headers: { authorization: `token ${mockParams.repo_token}` }
+    })
+  })
+
+  it('Should send client payload in request', () => {
+    require('./index')
+    expect(request.mock.calls[0][1]).toMatchObject({
+      client_payload: JSON.parse(mockParams.client_payload)
+    })
+  })
+
+  it('Should send event type in request', () => {
+    require('./index')
+    expect(request.mock.calls[0][1]).toMatchObject({
+      event_type: mockParams.event_type
+    })
+  })
+})
